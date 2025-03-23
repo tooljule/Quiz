839e0e44efa31c8d9f52cbdb4a59d839f3b35ac9
tooljule <144605405+tooljule@users.noreply.github.com>
2025-03-23 12:17:14 +0100

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,3 +1,22 @@
-self.addEventListener("fetch", function(event) {
-  console.log(`start server worker`)
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('pwa-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/script.js',
+        '/icons/icon-192x192.png',
+        '/icons/icon-512x512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
